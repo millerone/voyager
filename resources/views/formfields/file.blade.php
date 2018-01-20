06aef196f3cadd4e63b43e2fc9a7bbf8e11a742a
Francesco Mugnai <mugnai@gmail.com>
2018-01-20 12:14:42 +0100

    Update file.blade.php

--- resources/views/formfields/file.blade.php
+++ resources/views/formfields/file.blade.php
@@ -1,10 +1,26 @@
-@if(isset($dataTypeContent->{$row->field}))
-    @if(json_decode($dataTypeContent->{$row->field}))
-        @foreach(json_decode($dataTypeContent->{$row->field}) as $file)
-            <br/><a class="fileType" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}"> {{ $file->original_name ?: '' }} </a>
-        @endforeach
-    @else
-        <a class="fileType" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($dataTypeContent->{$row->field}) }}"> Download </a>
+
+@if($row->field == 'pdf')
+    @if( $dataTypeContent->getPdfPath() != null )
+            @foreach(json_decode($dataTypeContent->getPdfPath()) as $file)
+                <br/><a class="fileType" id="pdf_i18n_link" data-wine-id="{{$dataTypeContent->id}}" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}"> {{ $file->original_name ?: '' }} </a>
+            @endforeach
+        
     @endif
+    @else
+    @if(isset($dataTypeContent->{$row->field}))
+        @if(json_decode($dataTypeContent->{$row->field}))
+            @foreach(json_decode($dataTypeContent->{$row->field}) as $file)
+                <br/><a class="fileType" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}"> {{ $file->original_name ?: '' }} </a>
+            @endforeach
+        @else
+            <a class="fileType" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($dataTypeContent->{$row->field}) }}"> Download </a>
+        @endif
+    @endif
+
+    
+
+@endif
+@if($row->field == 'pdf')
+<input type="hidden"  name="pdf_original" value="{{$dataTypeContent->pdf}}"/>
 @endif
 <input @if($row->required == 1 && !isset($dataTypeContent->{$row->field})) required @endif type="file"  data-name="{{ $row->display_name }}"  name="{{ $row->field }}[]" multiple="multiple">
