a2d2866da251d15c5e5b8161b9d3857ab9a18419
Francesco Mugnai <mugnai@gmail.com>
2018-02-27 16:36:07 +0100

    Update file.blade.php

--- resources/views/formfields/file.blade.php
+++ resources/views/formfields/file.blade.php
@@ -1,8 +1,13 @@
 
+<input type="hidden" data-wine-id="{{$dataTypeContent->id}}" id="wine-ghost-id"/>
+<div id="pdf-link-admin">
 @if($row->field == 'pdf')
     @if( $dataTypeContent->getPdfPath() != null )
+            
             @foreach(json_decode($dataTypeContent->getPdfPath()) as $file)
+            {{--  {{dd($dataTypeContent->id)}}  --}}
                 <br/><a class="fileType" id="pdf_i18n_link" data-wine-id="{{$dataTypeContent->id}}" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}"> {{ $file->original_name ?: '' }} </a>
+                <a class="icon voyager-trash deletePdf" href="javascript:void(0)"></a>
             @endforeach
         
     @endif
@@ -20,7 +25,9 @@
     
 
 @endif
+</div>
 @if($row->field == 'pdf')
 <input type="hidden"  name="pdf_original" value="{{$dataTypeContent->pdf}}"/>
 @endif
+
 <input @if($row->required == 1 && !isset($dataTypeContent->{$row->field})) required @endif type="file"  data-name="{{ $row->display_name }}"  name="{{ $row->field }}[]" multiple="multiple">
